f8cbc1c91d7e68cd0da63993c7cc2465978d33ca
geckmare2759 <geckmare2759@gmail.com>
2023-05-08 22:33:08 +1000

    1.cleaner solution 2; 2.script to formatting python files;

---
+++ format.bat
@@ -0,0 +1,1 @@
+black solutions/list/*.py --line-length 80
--- solutions/list/merge_sorted_array.py
+++ solutions/list/merge_sorted_array.py
@@ -1,5 +1,6 @@
 # 88. Merge Sorted Array
 
+
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         """
@@ -35,19 +36,48 @@ class Solution(object):
                 i2 -= 1
 
             i -= 1
-        
+
         while i2 >= 0:
             n2 = nums2[i2]
             nums1[i] = n2
-            i2 -= 1 
-            i -= 1  
+            i2 -= 1
+            i -= 1
 
         while i1 >= 0:
             n1 = nums1[i1]
             nums1[i] = n1
-            i1 -= 1 
-            i -= 1  
+            i1 -= 1
+            i -= 1
 
-        # print(["final:", nums1])
-        # return nums1
-
+    def merge_one_while(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        if len(nums1) == 0:
+            return nums2
+        if len(nums2) == 0:
+            return nums1
+        i1 = m - 1
+        i2 = n - 1
+        i = m + n - 1
+        while i >= 0:
+            if i2 < 0:
+                nums1[i] = nums1[i1]
+                i1 -= 1
+            elif i1 < 0:
+                nums1[i] = nums2[i2]
+                i2 -= 1
+            else:
+                n2 = nums2[i2]
+                n1 = nums1[i1]
+                if n2 <= n1:
+                    nums1[i] = n1
+                    i1 -= 1
+                else:
+                    nums1[i] = n2
+                    i2 -= 1
+            i -= 1
